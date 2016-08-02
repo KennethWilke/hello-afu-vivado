bb92dcd068660edd4761ba5e16c0d5fe92d3f43e
KennethWilke <kenneth.wilke@rackspace.com>
2016-08-02 12:18:45 -0500

    fixy fix

--- sim.sh
+++ sim.sh
@@ -7,5 +7,6 @@ fi
 
 rm -r xelab.* xsc.* xsim.dir/ xvlog.*
 xsc $PSLSEDIR/afu_driver/src/afu_driver.c
-xvlog --sv *.sv
-xelab -timescale 1ns/1ps -svlog *.sv *.v -sv_root . -sv_lib libdpi -debug all
+xvlog --sv *.sv *.v
+xelab -timescale 1ns/1ps -svlog $PSLSEDIR/afu_driver/verilog/top.v -sv_root . -sv_lib libdpi -debug all
+xsim -g work.top
